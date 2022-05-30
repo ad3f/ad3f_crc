bad97f376e117970a89bd264961d25bf0e9ad04d
ad <admin@ad3f.me>
2022-05-30 23:35:52 +1000

    adjusted api for move to new backend

--- html/js/visitor_counter.js
+++ html/js/visitor_counter.js
@@ -1,5 +1,6 @@
 const counter = document.getElementById("visit_count");
-const url = "https://api.ad3f.me/stats/visit-count/";
+const url =
+  "https://go42ln7xp0.execute-api.ap-southeast-2.amazonaws.com/serverless_lambda_prod/api/visitors";
 let xhr = new XMLHttpRequest();
 
 //begin fetching our visitor count using our API
@@ -8,6 +9,5 @@ fetch(url)
     return res.json();
   })
   .then((data) => {
-    const res = data;
-    counter.textContent = Number(res.body.visit_count);
+    counter.textContent = data;
   });
